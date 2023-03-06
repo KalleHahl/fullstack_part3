dac219977f28f5c11764dfa29001ff03bcab7fd9
Kalle Hahl <kalle.hahl@hotmail.com>
2023-03-06 16:44:25 +0200

    tehtävä 3.22

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ mongoose.connect(url)
 
 const Person = require('./models/persons')
 
-morgan.token('data',(req,res) => {
-    if (req === 'POST') {
-        JSON.stringify(req.body)
-    }
+morgan.token('data',(req) => {
+  if (req === 'POST') {
+    JSON.stringify(req.body)
+  }
 })
 
 app.use(express.json())
@@ -25,83 +25,83 @@ app.use(express.static('build'))
 
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 
-app.get('/info', (request, response, next) => {
-    const date = new Date()
-    Person.find({}).then(persons => {
-        response.send(
-            `<div>phonebook has info for ${persons.length} people</div>
+app.get('/info', (request, response) => {
+  const date = new Date()
+  Person.find({}).then(persons => {
+    response.send(
+      `<div>phonebook has info for ${persons.length} people</div>
             <div>${date}</div>
             `
-        )
-    })
+    )
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    }) 
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save()
+  person.save()
     .then(savedPerson => {
-        res.json(savedPerson)
+      res.json(savedPerson)
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-      }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true , runValidators: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true , runValidators: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {3001
-    console.log(`Server runnning on port ${PORT}`)
+  console.log(`Server runnning on port ${PORT}`)
 })
--- backend/mongo.js
+++ backend/mongo.js
@@ -4,46 +4,46 @@ const mongoose = require('mongoose')
 const info = process.argv
 console.log(info)
 
-const url = `mongodb+srv://kallehahl:${info[2]}@cluster0.2ombmsk.mongodb.net/phonebookApp?retryWrites=true&w=majority` 
+const url = `mongodb+srv://kallehahl:${info[2]}@cluster0.2ombmsk.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    number: String,
+  id: Number,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (info.length === 3) {
-    mongoose
-        .connect(url)
-        .then(() => {
-            console.log('phonebook:')
-            Person
-                .find({})
-                .then(persons => {
-                    for (x in persons) {
-                        console.log(`${persons[x].name} ${persons[x].number}`)
-                    }
-                    mongoose.connection.close()
-                })
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log('phonebook:')
+      Person
+        .find({})
+        .then(persons => {
+          for (let x in persons) {
+            console.log(`${persons[x].name} ${persons[x].number}`)
+          }
+          mongoose.connection.close()
         })
-        
+    })
+
 } else if (info.length === 5) {
-    mongoose
-        .connect(url)
-        .then(() => {
-           person =  new Person({
-                name: info[3],
-                number: info[4]
-            })
-            return person.save()   
-        })
-        .then(() => {
-            console.log(`Added ${info[3]} number ${info[4]} to phonebook`)
-            return mongoose.connection.close()
-        })
+  mongoose
+    .connect(url)
+    .then(() => {
+      let person =  new Person({
+        name: info[3],
+        number: info[4]
+      })
+      return person.save()
+    })
+    .then(() => {
+      console.log(`Added ${info[3]} number ${info[4]} to phonebook`)
+      return mongoose.connection.close()
+    })
 }
 
